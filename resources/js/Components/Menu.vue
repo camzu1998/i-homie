efc30ee6d9f794a79dd71fcc4d8d8c1347952d5e
Kamil Langer <kamil.langer@caops.pl>
2024-02-27 22:53:12 +0100

    Improve top menu design and added notifications

--- resources/js/Components/Menu.vue
+++ resources/js/Components/Menu.vue
@@ -13,6 +13,7 @@
             default: false,
         }
     });
+    console.log(props.routes);
     const sidebarClass = 'w-100';
 </script>
 
@@ -20,12 +21,25 @@
     <div class="flex w-100 nav-justified" v-if="!props.sidebar">
         <ul class="nav">
             <MenuLink
-                v-for="route in props.routes"
+                v-for="route in props.routes.slice(0, 3)"
                 :href="route.path"
                 :name="route.name"
                 :active="route.path === this.$route.path ? 'active' : ''"
                 :hidden="route.meta.sidebar || (route.meta.auth && !this.$store.state.isLoggedIn) || (!route.meta.auth && this.$store.state.isLoggedIn)"
             />
+            <li class="nav-item" v-if="this.$store.state.isLoggedIn">
+                <div class="row">
+                    <div class="col-10"></div>
+                    <div class="col-2 row">
+                        <div class="col-6">
+                            <a class="nav-link fs-5 me-2" aria-current="page" href="#"><i class="fa-regular fa-envelope"></i></a>
+                        </div>
+                        <div class="col-6">
+                            <a class="nav-link fs-5 text-info" aria-current="page" href="/profile"><i class="fa-regular fa-user"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </li>
             <Logout v-if="this.$store.state.isLoggedIn" />
         </ul>
     </div>
